63217b524cc0fa06c9384f58234191d34596c1ec
Uwe Fechner <fechner@aenarete.eu>
2024-08-23 16:37:04 +0200

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ include("test_steady_state.jl")
 lift, drag = KiteModels.lift_drag(kps)
 
 @testset "PyKiteModels" begin
-    @test isapprox(lift, 563.1301363894148; atol=56)
+    @test isapprox(lift, 434.25661213474916; atol=56)
     @test isapprox(drag, 127.91137175090972; atol=28)
 
 end
